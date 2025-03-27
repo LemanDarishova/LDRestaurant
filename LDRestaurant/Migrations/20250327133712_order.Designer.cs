2377af34e386d52509172af627fbc20a202e1c2a
leylaheydarova <lbadalzade@gmail.com>
2025-03-27 18:20:01 +0400

    End of the project

--- LDRestaurant/Migrations/20250327133712_order.Designer.cs
+++ LDRestaurant/Migrations/20250327133712_order.Designer.cs
@@ -4,6 +4,7 @@ using LDRestaurant.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LDRestaurant.Migrations
 {
     [DbContext(typeof(LDRestaurantDbContext))]
-    partial class LDRestaurantDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250327133712_order")]
+    partial class order
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
